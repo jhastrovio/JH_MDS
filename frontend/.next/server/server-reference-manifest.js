4d1d64c1e141546688f0d73b3b6edf07df852610
jhastrovio <james.hassett@trovio.io>
2025-05-24 07:30:22 +0800

    chore: remove build artifacts from clean-baseline

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dB8eou2GGAyASrBW3p2fA/Cty9DzQZfw34+0yeB9WNY=\"\n}"
--- redis/start-redis.ps1
+++
@@ -1,3 +0,0 @@
-﻿# Start Redis Server
-Write-Host "Starting Redis Server..." -ForegroundColor Green
-& "C:\Users\JamesHassett\dev\JH-MDS\redis-server.exe" "C:\Users\JamesHassett\dev\JH-MDS\redis.conf"
