f29059f8b7b6bad89d4843b46f35b22032d85af7
jhastrovio <james.hassett@trovio.io>
2025-05-26 18:47:50 +0800

    Remove api_gateway mode from Vercel handler in index.py to enhance compatibility with FastAPI.

--- api/index.py
+++ api/index.py
@@ -2,4 +2,4 @@ from mangum import Mangum
 from app.app import app    # ← grab the FastAPI instance you just created
 
 # ensure the full /api/... path is preserved
-handler = Mangum(app, api_gateway=True)
+handler = Mangum(app)
